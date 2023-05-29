bc9c03ae1dde8174fde1f6df76b13fbafa77380e
Daniel González García <danielgonnet@gmail.com>
2023-05-29 16:49:21 +0200

    no more skeleton

--- test/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest"
-
-describe("Checking if everything", () => {
-	it("floats", () => {
-		console.log("TEST", process.env.DOCKER_HOST)
-		expect(false).to.be.false
-	})
-})
